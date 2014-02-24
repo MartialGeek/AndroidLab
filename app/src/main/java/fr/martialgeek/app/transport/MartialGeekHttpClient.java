50a8dc51a163850ddc36165ed218a3ea34275d70
Martial Saunois <saunois.martial@gmail.com>
2014-02-24 11:05:31 +0100

    Doc added.

--- app/src/main/java/fr/martialgeek/app/transport/MartialGeekHttpClient.java
+++ app/src/main/java/fr/martialgeek/app/transport/MartialGeekHttpClient.java
@@ -13,22 +13,39 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 
+/**
+ * Abstract class to request the MartialGeek webservices.
+ * @author Martial Saunois
+ */
 public abstract class MartialGeekHttpClient extends AsyncTask<Void, Void, String> {
     protected String mUrl;
     protected AndroidHttpClient mHttpClient;
     protected WeakReference<Fragment> mWeakReferenceFragment;
 
+    /**
+     * Constructor.
+     * @param fragment The fragment instance
+     * @param url The target URL
+     */
     public MartialGeekHttpClient(Fragment fragment, String url) {
         mWeakReferenceFragment = new WeakReference<Fragment>(fragment);
         mUrl = url;
     }
 
+    /**
+     * Prepare the HTTP client.
+     */
     @TargetApi(Build.VERSION_CODES.FROYO)
     @Override
     protected void onPreExecute() {
         mHttpClient = AndroidHttpClient.newInstance("Mozilla 5.0");
     }
 
+    /**
+     * Execute the request and returns the response.
+     * @param params The parameters send by the method execute().
+     * @return The string representation of the response.
+     */
     @TargetApi(Build.VERSION_CODES.FROYO)
     @Override
     protected String doInBackground(Void... params) {
@@ -43,9 +60,17 @@ public abstract class MartialGeekHttpClient extends AsyncTask<Void, Void, String
         return null;
     }
 
+    /**
+     * This task contains the logic to update the view with the result.
+     * @param response The HTTP response
+     */
     @Override
     protected abstract void onPostExecute(String response);
 
+    /**
+     * Get the fragment instance.
+     * @return The fragment instance
+     */
     protected Fragment getPlaceholderFragment() {
         return mWeakReferenceFragment.get();
     }
